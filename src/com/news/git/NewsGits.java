74c9e66cb8682c73fab8d05732c762b20edfd8b9
vikrant <vikrantbhendarkar93@gmail.com>
2024-04-09 12:22:21 +0530

    commiting hff

--- src/com/news/git/NewsGits.java
+++ src/com/news/git/NewsGits.java
@@ -1,8 +1,9 @@
 package com.news.git;
 
 public class NewsGits {
-public static void main(String[] args) {
-	System.out.println("Tushar");
-	System.out.println("Bramhankar");
-}
+	public static void main(String[] args) {
+		System.out.println("Tushar");
+		System.out.println("Bramhankar");
+		System.out.println("hff");
+	}
 }
